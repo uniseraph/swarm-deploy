fe4734a6855d444b65dcf022c095f6e9e3a4261b
zhengtao.wuzt <zhengtao.wuzt@alibaba-inc.com>
2016-11-14 15:46:50 +0800

    add daemon-reload

--- init-node.sh
+++ init-node.sh
@@ -13,8 +13,8 @@ echo "overlay" > /etc/modules-load.d/overlay.conf
 DOCKER_LISTEN_URL=$(ifconfig eth0 | grep inet | awk '{{print $2}}'):2375
 
 
-sed -i  "s/daemon/daemon -H unix:\/\/\/var\/run\/docker.sock -H ${DOCKER_LISTEN_URL}/g" -e /usr/lib/systemd/system/docker.service
+sed -i  "s#daemon#daemon -H unix:///var/run/docker.sock -H ${DOCKER_LISTEN_URL}#g" -e /usr/lib/systemd/system/docker.service
 
-
+systemctl deamon-reload
 
 systemctl restart docker
