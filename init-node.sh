3d683668274043f6496b6bbcb46ef11bdb19b9f4
zhengtao.wuzt <zhengtao.wuzt@alibaba-inc.com>
2016-11-14 15:50:27 +0800

    add daemon-reload

--- init-node.sh
+++ init-node.sh
@@ -1,6 +1,9 @@
 
 
-yum remove docker
+echo "deleting all containers ..."
+
+docker rm -f `docker ps -aq`
+yum remove -y   docker
 yum install -y  docker
 
 
@@ -13,7 +16,7 @@ echo "overlay" > /etc/modules-load.d/overlay.conf
 DOCKER_LISTEN_URL=$(ifconfig eth0 | grep inet | awk '{{print $2}}'):2375
 
 
-sed -i  "s#daemon#daemon -H unix:///var/run/docker.sock -H ${DOCKER_LISTEN_URL}#g" -e /usr/lib/systemd/system/docker.service
+sed -e  "s#daemon#daemon -H unix:///var/run/docker.sock -H ${DOCKER_LISTEN_URL}#g" -i /usr/lib/systemd/system/docker.service
 
 systemctl deamon-reload
 
