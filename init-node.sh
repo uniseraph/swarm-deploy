874b3c138d59f845a704211348fe3bc937e29cb1
zhengtao.wuzt <zhengtao.wuzt@alibaba-inc.com>
2016-11-14 15:33:25 +0800

    add :2375

--- init-node.sh
+++ init-node.sh
@@ -1,7 +1,6 @@
 
 
-
-
+yum remove docker
 yum install -y  docker
 
 
@@ -9,6 +8,13 @@ echo "DOCKER_STORAGE_OPTIONS=-s overlay" >> /etc/sysconfig/docker-storage
 
 modprobe overlay
 echo "overlay" > /etc/modules-load.d/overlay.conf
+
+
+DOCKER_LISTEN_URL=$(ifconfig eth0 | grep inet | awk '{{print $2}}'):2375
+
+
+sed -i  "s/daemon/daemon -H unix:\/\/\/var\/run\/docker.sock -H ${DOCKER_LISTEN_URL}/g" -e /usr/lib/systemd/system/docker.service
+
 
 
 systemctl restart docker
