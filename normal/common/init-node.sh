0a52cfe1a4080d31015983ca8b53b530f36a5758
Zhengtao Wu <wuzhengtao@ZhengtaodeMacBook-Pro.local>
2017-01-10 15:05:42 +0800

    add mut

--- normal/common/init-node.sh
+++ normal/common/init-node.sh
@@ -5,7 +5,7 @@ DEFAULT_IP=$( ifconfig eth0 | grep inet | awk '{{print $2}}' )
 CLUSTER_STORE=${CLUSTER_STORE:-"zk://${DEFAULT_IP}:2181/default"}
 
 CLUSTER_ADVERTISE=${CLUSTER_ADVERTISE:-"eth0:2376"}
-
+MTU=${MTU:-"1450"}
 
 rpm -i ../binary/acs-docker-1.11.1-5ec4436.x86_64.rpm
 
@@ -13,4 +13,9 @@ modprobe overlay openvswitch
 echo "overlay" > /etc/modules-load.d/overlay.conf
 echo "openvswitch" > /etc/modules-load.d/openvswitch.conf
 
-echo "DOCKER_OPTS='--cluster-store=${CLUSTER_STORE} --cluster-advertise=${CLUSTER_ADVERTISE}'" > /etc/default/docker
+echo "DOCKER_OPTS='--mtu=1450 --cluster-store=${CLUSTER_STORE} --cluster-advertise=${CLUSTER_ADVERTISE}'" > /etc/default/docker
+
+
+systemctl daemon-reload
+
+systemctl restart docker
